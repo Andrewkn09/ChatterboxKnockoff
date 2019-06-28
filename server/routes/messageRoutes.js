320e5c19941c58efcdb1475757eeca72f35567ed
Andrew Kan <Andrewkn09@Gmail.com>
2019-06-27 22:57:15 -0400

    Fix proccess environment port

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,14 +1,5 @@
 const router = require('express').Router();
 
-// const messageController = require('../controllers/messageControllers.js');
-
-// router.get('/rooms', messageController.Rooms.getRooms);
-// router.post('/rooms', messageController.Rooms.addRoom);
-// router.post('/users', messageController.Users.addUser);
-// router.get('/messages/:roomname', messageController.Messages.getMessages);
-// router.post('/messages', messageController.Messages.addMessage);
-
-//MONGODB =============================================
 const messageController = require('../controllers/messageControllers.js');
 router.get('/messages/:roomname', messageController.getMessages);
 router.post('/messages', messageController.saveMessage);
